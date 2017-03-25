6aaac380511af5a7a7710d19e952aa1c833b81e7
terpster <ktn1710@gmail.com>
2017-03-25 19:08:55 +0100

    sick commit

--- AarhusWebDevCoop/App_Data/Models/all.generated.cs
+++ AarhusWebDevCoop/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "47f06a72cc98f37e")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.7")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
